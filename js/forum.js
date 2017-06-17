e700c1b4b4d920b2dc73ac3b656d91f8d7f0992a
DreamingTraveler <dreaming34523@gmail.com>
2017-06-17 23:24:22 +0800

    Add article editor

--- js/forum.js
+++ js/forum.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
+    var editor = CKEDITOR.replace('content');
+
+    var dbForum = firebase.database().ref().child('forum');
+
     const $articleList = $('#article-list');
     const $publishBtn = $('#publish-btn');
+    const $submitBtn = $('#submit-btn');
+    const $articleTitle = $('#title-area');
+    const $articleContent = $('#article-content');
+
+    var content;
+    editor.on('change', function(evt){
+      console.log(evt.editor.getData());
+      content = evt.editor.getData();
+    })
 
     $publishBtn.click(function(e){
       firebase.auth().onAuthStateChanged(function(currentUser){
@@ -15,5 +28,15 @@ $(document).ready(function(){
           window.location.href = "log_in.html";
         }
       });
+    });
+
+    $submitBtn.click(function(e){
+      currentUser = firebase.auth().currentUser;
+      console.log($articleTitle[0].value);
+      console.log(content);
+      $(".article").html(content);
+      //location.search = currentUser.uid;
+      console.log(location.search);
+      //window.location.href = "article.html";
     });
 });
