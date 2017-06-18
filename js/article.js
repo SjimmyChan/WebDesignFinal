909c5bacf1918dcb1346895fc16e069e4ace5b7f
DreamingTraveler <dreaming34523@gmail.com>
2017-06-18 16:32:42 +0800

    FORUM ver1.0 is available new

--- js/article.js
+++ js/article.js
@@ -3,13 +3,18 @@ $(document).ready(function(){
     var dbUser = firebase.database().ref().child('user');
 
     dbForum.on("value", function(snapshot){
+      var forumId = dbForum.child(snapshot.key).key;
+      //location.search = forumId;
       snapshot.forEach(function(data){
-        console.log(data.val().title);
-        $('.article-title').html(data.val().title);
-        $('.article-title').css({
-          "font-size" : "30px"
-        });
-        $('.article').html(data.val().content);
+        console.log(dbForum.child(data.key).key);
+        if(location.search === '?' + dbForum.child(data.key).key){
+          $('.article-title').html(data.val().title);
+          $('.article-title').css({
+            "font-size" : "30px"
+          });
+          $('.article').html(data.val().content);
+        }
+
       });
     });
 });
